f7918c9ca7f5f6c9900a3ac6ecaa8b9fc7ea19e6
thanyaboonaumpong <thanyaboonaumpong@hotmail.com>
2026-05-04 14:52:36 +0700

    note-html-jsx_homepage-userprofilepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,6 @@
+// TODO: แปลงหน้า Home จาก html/index.html เป็น React
+// - แยก Hero section
+// - แยก Product section
+// - แยก Calculator section
+// - แยก Footer
+// - ต้องปรับภาพและ path ให้ตรง src/assets
