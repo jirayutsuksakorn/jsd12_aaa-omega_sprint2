edc249042635b427f5b0ebf33ef5867cbf972bfb
jirayutsuksakorn <jirayutsuksakorn@gmail.com>
2026-05-08 21:09:45 +0700

    react-component-v.02

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,30 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-// import { MessageProvider } from "./contexts/messageContext/MessageProvider";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <MessageProvider>
-//       <App />
-//     </MessageProvider>
-//   </StrictMode>
-// );
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './assets/css/App.css';
-import AppTest from "./AppTest";
+import App from "./App.jsx";
+import { MessageProvider } from "./contexts/messageContext/MessageProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppTest />
+    <MessageProvider>
+      <App />
+    </MessageProvider>
   </StrictMode>
 );
+
+
+
+
+// // Test for Homepage(CARD)
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import './assets/css/App.css';
+// import AppTest from "./AppTest";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <AppTest />
+//   </StrictMode>
+// );
+
+// // Test for Homepage(CARD)
